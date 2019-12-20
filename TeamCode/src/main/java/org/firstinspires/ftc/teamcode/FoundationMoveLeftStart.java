1b0e53b460716047d03d7019f2c2bd40b6f0bd87
SilkPDX <ethanlupdx@gmail.com>
2019-12-20 08:29:29 -0800

    added autonomous for read team foundation from left and right start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationMoveLeftStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationMoveLeftStart.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name = ":FoundationMoveRightStart", group = "Autonomous")
-public class FoundationMoveRightStart extends AutonomousFramework {
+public class FoundationMoveLeftStart extends AutonomousFramework {
     @Override
     public void runOpMode() throws InterruptedException {
         runSetup();
+        turnDegrees(1,90);
+        driveCm(1,176,176);
+        turnDegrees(1,-90);
         turnDegrees(1,25);
         driveCm(1,82,82);
         turnDegrees(1, -25);
