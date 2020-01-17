19dcef89d69c35d913509a3052025bb94eb6a10b
SilkPDX <ethanlupdx@gmail.com>
2020-01-17 10:48:40 -0800

    Added bridge park ability to foundation mover opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlueLStartBridgePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlueLStartBridgePark.java
@@ -6,36 +6,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class FoundationBlueLStartBridgePark extends AutonomousFramework {
     @Override
     public void runOpMode() throws InterruptedException {
-        runSetup();
-        driveCm(1, 10, 10);
-        turnDegrees(-1, 45);
-        driveCm(1, 50, 50);
-        turnDegrees(-1, 150);
-        driveCm(-0.5, 30, 30);
+        driveCm(1,10,10);
+        turnDegrees(-1,45);
+        driveCm(1,50,50);
+        turnDegrees(-1, 145);
+        driveCm(-0.5,30,30);
         moveFoundationMover(0.8);
         sleep(800);
-        driveCm(1, 50, 50);
+        driveCm(1,40,40);
         moveFoundationMover(0);
         sleep(800);
-        driveCm(1, 10, 10);
-        turnDegrees(-1, 95);
-        driveCm(1, 70, 70);
-        turnDegrees(-1, 90);
-        driveCm(1, 120, 120);
-        turnDegrees(-1, 90);
-        driveCm(1, 70, 70);
-        turnDegrees(1, 90);
-        driveCm(-0.5, 20, 20);
+        driveCm(1,10,10);
+        turnDegrees(-1,95);
+        driveCm(1,70,70);
+        turnDegrees(-1,90);
+        driveCm(1,120,120);
+        turnDegrees(-1,100);
+        driveCm(1, 70,70);
+        turnDegrees(1,90);
+        driveCm(-0.5,30,30);
         moveFoundationMover(0.8);
-        driveCm(-1, 40, 40);
+        driveCm(-1,50,50);
         moveFoundationMover(0);
 
-        driveCm(1, 20, 20);
-        turnDegrees(1, 90);
-        driveCm(1, 70, 70);
-        turnDegrees(1, 90);
-        driveCm(1, 35, 35);
-        turnDegrees(-1, 90);
-        driveCm(1, 50, 50);
+        driveCm(1,10,10);
+        turnDegrees(1,90);
+        driveCm(1,70,70);
+        turnDegrees(1,90);
+        driveCm(1,50,50);
+        turnDegrees(-1,75);
+        driveCm(1,20,20);
     }
 }
